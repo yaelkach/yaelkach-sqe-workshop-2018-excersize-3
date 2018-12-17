c5cf8114ef4bee95e54f5f00c615a5e39060f6d2
yaelkach <yaelkach@post.bgu.ac.il>
2018-12-17 20:28:15 +0200

    need to check last coverage

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,14 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
-import {main, tab} from './code-analyzer';
+//import {parseCode} from './code-analyzer';
+import {Program} from './code-analyzer';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let arg =  $('#arguments').val();
-        let parsedCode = parseCode(codeToParse);
-        let parsedArguments;
-
-      //  console.log(parsedArguments);
-       // console.log(parsedCode);
-        let arr = main(codeToParse, arg);
-       // console.log(arr);
-        // let table = tab(arr);
-        // $('#table').empty();
-        // $('#table').append(table);
-        console.log(arr);
-      //  $('#parsedCode').val(JSON.stringify(arr, null, 2));
-        $('#parsedCode').val(arr);
+        let colors = [];
+        let arr = Program(codeToParse, arg,colors);
+        $('#parsedCode').html(arr);
 
     });
 });
--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -1,84 +1,232 @@
 import assert from 'assert';
-import {parseCode, main, tab} from '../src/js/code-analyzer';
+import {main, Program} from '../src/js/code-analyzer';
 
 
 describe('The javascript parser', () => {
-    it('is parsing a function with var, return, assignment, while, if and else if correctly', () => {
+    it('testing program without parenthesess', () => {
         assert.deepEqual(
-            main(('function foo(x, y, z){\n' +
-        'let a = x + 1;\n' +
-        'let b = a + y;\n' +
-        'let c = 0;\n' +
-        'if (b < z) {\n' +
-         '   c = c + 5;\n' +
-          '  return x + y + z + c;\n' +
-        '} else if (b < z * 2) {\n' +
-          '  c = c + x + 5;\n' +
-          '  return x + y + z + c;\n'+
-       '} else {\n'+
-          '  c = c + z + 5;\n'+
-           ' return x + y + z + c;\n'+
-       ' }'), '(X=1, y=2, z=3)'), 'function foo(x, y, z) {\n' +
-        '    if (x + 1 + y < z) {\n' +
-        '        return x + y + z + (0 + 5);\n' +
-        '    } else if (x + 1 + y < z * 2) {\n' +
-        '        return x + y + z + (0 + x + 5);\n' +
-        '    } else {\n' +
-        '        return x + y + z + (0 + z + 5);\n' +
-        '    }\n' +
-        '}');
+            Program('let count;       \n' +
+                'function loo(x,y){\n' +
+                'count =0;             \n' +
+                'while(count<y)             \n' +
+                'count = count +1;\n' +
+                '                 \n' +
+                'if(x>y)\n' +
+                'count = count+2;\n' +
+                'else if(x<y)\n' +
+                'count = count+3;\n' +
+                'else\n' +
+                'count = count+4;\n' +
+                '\n' +
+                'return z;\n' +
+                '                }', '(x=6, y=4)', []), '<pre>\n' +
+            'let count;\n' +
+            '\n' +
+            'function loo(x, y) {\n' +
+            '\n' +
+            '    count = 0;\n' +
+            '\n' +
+            '<span style="background-color:green;">     while (count < y) </span>\n' +
+            '\n' +
+            '        count = count + 1;\n' +
+            '\n' +
+            '<span style="background-color:green;">     if (x > y) </span>\n' +
+            '\n' +
+            '        count = count + 2;\n' +
+            '\n' +
+            '<span style="background-color:red;">     else if (x < y) </span>\n' +
+            '\n' +
+            '        count = count + 3;\n' +
+            '\n' +
+            '    else\n' +
+            '\n' +
+            '        count = count + 4;\n' +
+            '\n' +
+            '    return;\n' +
+            '\n' +
+            '}\n' +
+            '</pre>');
+    });
+    it('testing program with parenthesess', () => {
+        assert.deepEqual(
+            Program('function loo(x,y){\n' +
+                'let c =0;             \n' +
+                'while(c<y){             \n' +
+                'count = c +1;\n' +
+                '     }            \n' +
+                'if(x>y){\n' +
+                'count = c+2;\n' +
+                '}\n' +
+                'else if(x<y){\n' +
+                'c = c+3;\n' +
+                '}\n' +
+                'else{\n' +
+                'c = c+4;\n' +
+                '}\n' +
+                '\n' +
+                'return z;\n' +
+                '                }', '(x=2, y=3)', []), '<pre>\n' +
+            'function loo(x, y) {\n' +
+            '\n' +
+            '<span style="background-color:green;">     while (0 < y){ </span>\n' +
+            '\n' +
+            '    }\n' +
+            '\n' +
+            '<span style="background-color:red;">     if (x > y){ </span>\n' +
+            '\n' +
+            '<span style="background-color:green;">     } else if (x < y){ </span>\n' +
+            '\n' +
+            '    } else {\n' +
+            '\n' +
+            '    }\n' +
+            '\n' +
+            '    return;\n' +
+            '\n' +
+            '}\n' +
+            '</pre>');
+    });
+    it('testing global init is null', () => {
+        assert.deepEqual(
+            main('let count;\n' +
+                'function moo(x,y){\n' +
+                'count =0;\n' +
+                'while(x<y){\n' +
+                'count = count +1;\n' +
+                '}\n' +
+                '}', '(x=6, y=4)', []), 'let count;\n' +
+            'function moo(x, y) {\n' +
+            '    count = 0;\n' +
+            '    while (x < y) {\n' +
+            '        count = count + 1;\n' +
+            '    }\n' +
+            '}');
+    });
+
+    it('testing global array', () => {
+        assert.deepEqual(
+            main('let arr = [1,2,3];\n' +
+                'function goo(x){\n' +
+                'if(x<arr[2]){\n' +
+                'return 1;\n' +
+                '}\n' +
+                '}', '(x=6)', []), 'let arr = [\n' +
+            '    1,\n' +
+            '    2,\n' +
+            '    3\n' +
+            '];\n' +
+            'function goo(x) {\n' +
+            '    if (x < arr[2]) {\n' +
+            '        return 1;\n' +
+            '    }\n' +
+            '}');
+    });
+    it('testing assignment arrays', () => {
+        assert.deepEqual(
+            main('function checkSameArr(arr){\n' +
+                'arr[0] = arr[1];\n' +
+                'let c=4;\n' +
+                'if(arr[0]<c){\n' +
+                'c = c+1;\n' +
+                'return c;\n' +
+                '}\n' +
+                'else{\n' +
+                'c=c+2;\n' +
+                'return arr[0];\n' +
+                '}\n' +
+                '}', '(arr=[1,2,3])', []), 'function checkSameArr(arr) {\n' +
+            '    arr[0] = arr[1];\n' +
+            '    if (arr[0] < 4) {\n' +
+            '        return 4 + 1;\n' +
+            '    } else {\n' +
+            '        return arr[0];\n' +
+            '    }\n' +
+            '}');
+    });
+
+    it('testing uninitialized variables', () => {
+        assert.deepEqual(
+            main('function foo(x, y, z){\n' +
+                '    let a = x + 1;\n' +
+                '    let b = a + y;\n' +
+                '    let c = 0;\n' +
+                'let d;\n' +
+                '    \n' +
+                '    while (a < z) {\n' +
+                '        c = a + b;\n' +
+                '        z = c * 2;\n' +
+                'd=0;\n' +
+                '    }\n' +
+                '    \n' +
+                '    return z;\n' +
+                '}\n', '(x=0, y=1, z=2)', []), 'function foo(x, y, z) {\n' +
+            '    while (x + 1 < z) {\n' +
+            '        z = (x + 1 + (x + 1 + y)) * 2;\n' +
+            '    }\n' +
+            '    return z;\n' +
+            '}');
+    });
+    it('testing arrays', () => {
+        assert.deepEqual(
+            main('function firstElementEqual(arr){\n' +
+                'let arr2 = [1,2,3];\n' +
+                'if(arr[0]<arr2[0])\n' +
+                'return 1;\n' +
+                'else\n' +
+                'return 0;\n' +
+                '}', '(arr=[1,2,4])', []), 'function firstElementEqual(arr) {\n' +
+            '    if (arr[0] < 1)\n' +
+            '        return 1;\n' +
+            '    else\n' +
+            '        return 0;\n' +
+            '}');
+    });
+    it('testing while', () => {
+        assert.deepEqual(
+            main('function foo(){\n' +
+                '    let a =2;\n' +
+                'while(a<6){\n' +
+                'a=a+1;\n' +
+                '}\n' +
+                '}\n', '', []), 'function foo() {\n' +
+            '    while (2 < 6) {\n' +
+            '    }\n' +
+            '}');
+    });
+    it('testing global with function', () => {
+        assert.deepEqual(
+            main('let global = 0;\n' +
+                'function addGlobal(x){\n' +
+                'global = global+x;\n' +
+                '}\n', '(x=1)', []), 'let global = 0;\n' +
+            'function addGlobal(x) {\n' +
+            '    global = global + x;\n' +
+            '}');
+    });
+    it('testing if and assignment', () => {
+        assert.deepEqual(
+            main('function foo(x, y, z){\n' +
+                '    let a = x + 1;\n' +
+                '    let b = a + y;\n' +
+                '    let c = 0;\n' +
+                '    \n' +
+                '    if (b < z) {\n' +
+                '        c = c + 5;\n' +
+                '        return x + y + z + c;\n' +
+                '    } else if (b < z * 2) {\n' +
+                '        c = c + x + 5;\n' +
+                '        return x + y + z + c;\n' +
+                '    } else {\n' +
+                '        c = c + z + 5;\n' +
+                '        return x + y + z + c;\n' +
+                '    }\n' +
+                '}\n', '(x=1, y=2, z=3)', []), 'function foo(x, y, z) {\n' +
+            '    if (x + 1 + y < z) {\n' +
+            '        return x + y + z + (0 + 5);\n' +
+            '    } else if (x + 1 + y < z * 2) {\n' +
+            '        return x + y + z + (0 + x + 5);\n' +
+            '    } else {\n' +
+            '        return x + y + z + (0 + z + 5);\n' +
+            '    }\n' +
+            '}');
     });
 });
-//
-// it('check for loop', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main('function increaseByFive(x){for(let i=0; i<5; i++){x = x+1;}return arr;}')),
-//         '[{"line":1,"type":"Function Declaration","name":"increaseByFive","condition":"","value":""},{"line":1,"type":"Variable Declaration","name":"x","condition":"","value":""},{"line":1,"type":"For Statement","name":"","condition":"i < 5","value":""},{"line":1,"type":"Assignment Expression","name":"x","condition":"","value":"x + 1"},{"line":1,"type":"Return Statement","name":"","condition":"","value":"arr"}]');
-// });
-//
-// it('check while with no parentheses', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main('function varDecCheck(){let low= 1;let high;}')),
-//         '[{"line":1,"type":"Function Declaration","name":"varDecCheck","condition":"","value":""},{"line":1,"type":"Variable Declaration","name":"low","condition":"","value":"1"},{"line":1,"type":"Variable Declaration","name":"high","condition":"","value":""}]');
-// });
-// it('check html table', () => {
-//     assert.deepEqual(
-//         JSON.stringify(tab(main('function varDecCheck(){let low= 1;let high;}'))),
-//         '"<table border=2><tr><td>Line</td><td>Type</td><td>Name</td><td>Condition</td><td>Value</td></tr><tr><td>1</td><td>Function Declaration</td><td>varDecCheck</td><td></td><td></td></tr><tr><td>1</td><td>Variable Declaration</td><td>low</td><td></td><td>1</td></tr><tr><td>1</td><td>Variable Declaration</td><td>high</td><td></td><td></td></tr></table>"');
-// });
-//
-// it('check empty function', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main('function empty(){}')),
-//         '[{"line":1,"type":"Function Declaration","name":"empty","condition":"","value":""}]');
-// });
-//
-// it('check empty input', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main(' ')),
-//         '[]');
-// });
-//
-// it('check if else with parentheses', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main('function isGreaterThanTen(x){if(x>10){return 1;}else {return 0;}}' )),
-//         '[{"line":1,"type":"Function Declaration","name":"isGreaterThanTen","condition":"","value":""},{"line":1,"type":"Variable Declaration","name":"x","condition":"","value":""},{"line":1,"type":"If Statement","name":"","condition":"x > 10","value":""},{"line":1,"type":"Return Statement","name":"","condition":"","value":"1"},{"line":1,"type":"Return Statement","name":"","condition":"","value":"0"}]');
-// });
-//
-// it('check else with parentheses', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main('function isGreaterThanTen(x){if(x>10){return 1;}else if(x<0){return 2;} else{ return 0;}}' )),
-//         '[{"line":1,"type":"Function Declaration","name":"isGreaterThanTen","condition":"","value":""},{"line":1,"type":"Variable Declaration","name":"x","condition":"","value":""},{"line":1,"type":"If Statement","name":"","condition":"x > 10","value":""},{"line":1,"type":"Return Statement","name":"","condition":"","value":"1"},{"line":1,"type":"Else-if Statement","name":"","condition":"x < 0","value":""},{"line":1,"type":"Return Statement","name":"","condition":"","value":"2"},{"line":1,"type":"Return Statement","name":"","condition":"","value":"0"}]');
-// });
-// it('check if else with no parentheses', () => {
-//     assert.deepEqual(
-//         JSON.stringify(main('function isGreaterThanTen(x){if(x>10)return 1;else return 0;}' )),
-//         '[{"line":1,"type":"Function Declaration","name":"isGreaterThanTen","condition":"","value":""},{"line":1,"type":"Variable Declaration","name":"x","condition":"","value":""},{"line":1,"type":"If Statement","name":"","condition":"x > 10","value":""},{"line":1,"type":"Return Statement","name":"","condition":"","value":"1"},{"line":1,"type":"Return Statement","name":"","condition":"","value":"0"}]');
-// });
-//
-// it('check codeParse', () => {
-//     assert.deepEqual(
-//         JSON.stringify(parseCode('function r(){return 1;}' )),
-//         '{"type":"Program","body":[{"type":"FunctionDeclaration","id":{"type":"Identifier","name":"r","loc":{"start":{"line":1,"column":9},"end":{"line":1,"column":10}}},"params":[],"body":{"type":"BlockStatement","body":[{"type":"ReturnStatement","argument":{"type":"Literal","value":1,"raw":"1","loc":{"start":{"line":1,"column":20},"end":{"line":1,"column":21}}},"loc":{"start":{"line":1,"column":13},"end":{"line":1,"column":22}}}],"loc":{"start":{"line":1,"column":12},"end":{"line":1,"column":23}}},"generator":false,"expression":false,"async":false,"loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":23}}}],"sourceType":"script","loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":23}}}');
-// });
-
