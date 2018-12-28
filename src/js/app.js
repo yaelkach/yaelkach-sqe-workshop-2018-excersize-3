7153da43b4c5704ef867d845e8ccec61993ee702
yaelkach <yaelkach@post.bgu.ac.il>
2018-12-28 11:03:38 +0200

    f

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,7 @@ import $ from 'jquery';
 //import {parseCode} from './code-analyzer';
 import {Program} from './code-analyzer';
 
-$(document).ready(function () {
-    $('#codeSubmissionButton').click(() => {
-        let codeToParse = $('#codePlaceholder').val();
-        let arg =  $('#arguments').val();
-        let colors = [];
-        let arr = Program(codeToParse, arg,colors);
-        $('#parsedCode').html(arr);
 
-    });
 });
 
 
